604466798ab5e8fd2364cada351f5c48d8d9d45d
Lanc3 <aaron.keating.lanc3@gmail.com>
2020-03-03 17:43:51 +0000

    added start of website for prod development

---
+++ precache-manifest.f3fa351ded1f0a2b6de32a8b0120e1b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b067932d3350fa1ad51fa9c8d36312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f2e02130b0db064a0e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f82250c61c50a2e03c65",
+    "url": "/static/js/2.1c78273f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1c78273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f2e02130b0db064a0e",
+    "url": "/static/js/main.514562d1.chunk.js"
+  },
+  {
+    "revision": "e45e20948a247110e75b",
+    "url": "/static/js/runtime-main.46b10d78.js"
+  }
+]);
